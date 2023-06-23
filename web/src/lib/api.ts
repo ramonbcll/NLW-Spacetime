2f1e11938089e3b66f7b6913af94deb635680d5f
Ramon Basto <42789151+ramonbcll@users.noreply.github.com>
2023-06-23 13:21:58 -0300

    Update api.ts

--- web/src/lib/api.ts
+++ web/src/lib/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'https://nlw-spacetime-back-end.vercel.app/',
+  baseURL: 'https://nlw-spacetime-back0end.vercel.app/',
 })
